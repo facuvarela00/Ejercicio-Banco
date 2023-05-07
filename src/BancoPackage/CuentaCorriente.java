1b0ee3d6301ad9559b296d1eeecea2d25013bb1b
MeliAbi <melina.abillera@gmail.com>
2023-05-07 16:21:28 -0300

    final?

--- src/BancoPackage/CuentaCorriente.java
+++ src/BancoPackage/CuentaCorriente.java
@@ -2,12 +2,45 @@ package BancoPackage;
 
 public class CuentaCorriente extends Cuenta{
 	
+	private Double descubierto;
+	private Double deuda;
+
 	public CuentaCorriente(Cliente nuevoCliente) {
 		super(nuevoCliente);
+		this.descubierto=0.0;
+		this.deuda=0.0;
 	}
 
-	// HOLA XD
-	// LOL XDE
-	//SARACATUNGA
+	public void setDescubierto(Double descubierto) {
+		this.descubierto=descubierto;
+		
+	}
+
+	public Double getDescubierto() {
+		return descubierto;
+	}
+	
+	@Override
+	public void extraerSaldo(Double saldoQueSeExtrae) {
+		Double descubiertoUtilizado=0.00;
+		
+		if (saldo>=saldoQueSeExtrae) {
+			saldo-=saldoQueSeExtrae;
+		}else if(saldo+descubierto>=saldoQueSeExtrae) {
+			descubiertoUtilizado=saldoQueSeExtrae-saldo;
+			descubierto-=(descubiertoUtilizado);
+			deuda+=(descubiertoUtilizado+descubiertoUtilizado*0.05);
+			saldo=0.0;		
+		}
+		
+	}
+
+	public Double getDeuda() {
+		return deuda;
+	}
+
+	public void setDeuda(Double deuda) {
+		this.deuda = deuda;
+	}
 
 }
--- test/BancoPackage/Test.java
+++ test/BancoPackage/Test.java
@@ -171,4 +171,70 @@ public class Test {
 		assertEquals(1,nuevoBanco.getListaDeCuentaCorriente().size());	
 	}
 	
+	@org.junit.Test
+	public void queSeApliqueDescubierto() {
+		//Inicializacion
+		String nombreBanco="xD";
+		String nombreCliente="Pedro", apellidoCliente="Pascal";
+		Integer dniCliente=42952902;
+		Double descubierto = 400.00;
+				
+				
+		//Instanciación
+		Banco nuevoBanco=new Banco(nombreBanco);
+		Cliente nuevoCliente=new Cliente(nombreCliente,apellidoCliente,dniCliente);
+		CuentaCorriente nuevaCuentaCorriente=new CuentaCorriente(nuevoCliente);
+		nuevoBanco.agregarCuentaCorrienteAlBanco(nuevaCuentaCorriente);
+		nuevaCuentaCorriente.setDescubierto(descubierto);	
+		
+		//Verificacion Que se cree descubierto
+		Double ve= 400.0;
+		assertEquals(ve,nuevaCuentaCorriente.getDescubierto());	
+		
+		//Verificacion Que se extraiga del descubierto
+		Double es= 1100.0;
+		nuevaCuentaCorriente.agregarSaldo(1000.0);
+		nuevaCuentaCorriente.extraerSaldo(es);
+		
+		ve= 300.0;
+		assertEquals(ve,nuevaCuentaCorriente.getDescubierto());	
+		
+		//Verificacion Que no extraiga
+		es= 500.0;
+		nuevaCuentaCorriente.extraerSaldo(es);
+				
+		ve= 300.0;
+		assertEquals(ve,nuevaCuentaCorriente.getDescubierto());	
+	}
+	
+	@org.junit.Test
+	public void queSeSumeDeuda() {
+		//Inicializacion
+		String nombreBanco="xD";
+		String nombreCliente="Pedro", apellidoCliente="Pascal";
+		Integer dniCliente=42952902;
+		Double descubierto = 400.00;
+				
+				
+		//Instanciación
+		Banco nuevoBanco=new Banco(nombreBanco);
+		Cliente nuevoCliente=new Cliente(nombreCliente,apellidoCliente,dniCliente);
+		CuentaCorriente nuevaCuentaCorriente=new CuentaCorriente(nuevoCliente);
+		nuevoBanco.agregarCuentaCorrienteAlBanco(nuevaCuentaCorriente);
+		nuevaCuentaCorriente.setDescubierto(descubierto);	
+		
+		//Verificacion Que se cree descubierto
+		Double ve= 400.0;
+		assertEquals(ve,nuevaCuentaCorriente.getDescubierto());	
+		
+		//Verificacion Que se sume deuda
+		Double es= 1100.0;
+		nuevaCuentaCorriente.agregarSaldo(1000.0);
+		nuevaCuentaCorriente.extraerSaldo(es);
+		
+		ve= 105.0;
+		assertEquals(ve,nuevaCuentaCorriente.getDeuda());	
+	}
+	
+	
 }
